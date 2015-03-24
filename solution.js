810cd38e75de322db274e729551cd805c81f04df
Ying Xiong <yxiong@seas.harvard.edu>
2015-03-23 21:32:18 -0400

    Simple solution, passed first challenage

--- solution.js
+++ solution.js
@@ -1,4 +1,18 @@
 {
-    init: function(elevators, floors) { },
+    init: function(elevators, floors) {
+        elevators.forEach(function (elevator) {
+            elevator.on("floor_button_pressed", function(floorNum) {
+                elevator.destinationQueue.push(floorNum);
+            });
+        });
+        floors.forEach(function (floor) {
+            floor.on("up_button_pressed", function() {
+                elevators[0].destinationQueue.push(floor.floorNum());
+            });
+            floor.on("down_button_pressed", function() {
+                elevators[0].destinationQueue.push(floor.floorNum());
+            });
+        });
+    },
     update: function(dt, elevators, floors) { }
 }
